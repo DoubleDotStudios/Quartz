099b7db323d093d1772dcc98d6ca771a9acd1720
DoubleDotGames <DoubleDotGames@gmail.com>
2024-05-04 16:53:08 +0100

    Updated DropdownMenu

--- src/renderer/src/components/Button/DropdownMenu.tsx
+++ src/renderer/src/components/Button/DropdownMenu.tsx
@@ -1,19 +1,34 @@
 import { setTheme } from '@renderer/App'
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Dropdown from 'react-bootstrap/Dropdown'
 import { FaMountainSun } from 'react-icons/fa6'
 
-export let style: string
-
 export const DropdownMenu = () => {
   const [isOpen, setIsOpen] = useState(false)
+  const dropdownRef = useRef(null)
 
   const toggleDropdown = () => {
-    setIsOpen(!isOpen)
+    setIsOpen((prevIsOpen) => !prevIsOpen)
   }
 
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
+        setIsOpen(false)
+      }
+    }
+
+    // Add event listener when component mounts
+    document.addEventListener('mousedown', handleClickOutside)
+
+    // Remove event listener when component unmounts
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside)
+    }
+  }, []) // Empty dependency array ensures this effect runs only once
+
   return (
-    <Dropdown onClick={toggleDropdown}>
+    <Dropdown onClick={toggleDropdown} ref={dropdownRef}>
       <Dropdown.Toggle
         id="Theme_Dropdown"
         className="px-2 py-1 rounded-md border border-zinc-400/50 hover:bg-zinc-600/50 transition-colors duration-100 flex justify-between mt-1"
@@ -21,7 +36,10 @@ export const DropdownMenu = () => {
         <FaMountainSun className="w-4 h-4 text-zinc-300" />
       </Dropdown.Toggle>
       {isOpen && (
-        <Dropdown.Menu className="px-2 py-1 rounded-md border transition-colors duration-100 flex justify-between mt-1 bg-zinc-800 flex-col">
+        <Dropdown.Menu
+          className="px-2 py-1 rounded-md border transition-colors duration-100 flex justify-between mt-1 bg-zinc-800 flex-col"
+          onClick={(e) => e.stopPropagation()}
+        >
           <Dropdown.Item onClick={() => setTheme('')}>Classic</Dropdown.Item>
           <Dropdown.Divider />
           <Dropdown.Item onClick={() => setTheme('amethyst')}>Amethyst</Dropdown.Item>
